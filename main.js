8826ed3f6e43485539012aeb32073d3f20d6b08f
sashicupcakes1 <85951962+sashicupcakes1@users.noreply.github.com>
2022-05-13 14:35:01 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+//https://teachablemachine.withgoogle.com/models/v_sl95BzE/
+Webcam.set({
+width:350,
+height:300,
+image_format : 'png',
+png_quality:90
+});
+
+camera = document.getElementById("camera");
+webcam.attach(  '#camera' );
+
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = '<img id="captured_image" src+"'+data_uri+'"/>';
+    });
+
+}
+
+console.log('ml5 version:', ml5.version);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/v_sl95BzE/model.json',modelLoaded);
+function modelLoaded(){
+    console.log("modelLoaded")
+}
